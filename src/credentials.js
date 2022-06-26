f583f90f7318227c3204eed63737c71d84635e14
Tahir Taim <77389557+tahir-taim@users.noreply.github.com>
2022-06-26 13:41:50 +0600

    Add files via upload

---
+++ src/credentials.js
@@ -0,0 +1,15 @@
+exports.FACEBOOK_EMAIL = "syedraihanali05@gmail.com";
+exports.FACEBOOK_PASSWORD = "Raihan$$1122";
+// Heroku application token
+exports.TOKEN = "d578ff34-b17e-438e-ac32-529792e7408d";
+// MemCachier credentials (from MemCachier dashboard)
+exports.MEMCACHIER_PASSWORD = "";
+exports.MEMCACHIER_SERVERS = "";
+exports.MEMCACHIER_USERNAME = "";
+// Spotify API credentials
+exports.SPOTIFY_CLIENTID = "";
+exports.SPOTIFY_CLIENTSECRET = "";
+// Wolfram short-answer API key
+exports.WOLFRAM_KEY = "";
+// Open Weather Map API key
+exports.WEATHER_KEY = "";
